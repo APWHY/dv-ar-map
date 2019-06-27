1057d9846fb649972795f04c99cb82df6de08b33
Thomas Kilkelly <thomas.kilkelly@bcgdv.com>
2019-06-27 12:43:08 +1000

    first commit

---
+++ app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageActivity.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ar.sceneform.samples.augmentedimage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Frame;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.samples.common.helpers.SnackbarHelper;
+import com.google.ar.sceneform.ux.ArFragment;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This application demonstrates using augmented images to place anchor nodes. app to include image
+ * tracking functionality.
+ */
+public class AugmentedImageActivity extends AppCompatActivity {
+
+  private ArFragment arFragment;
+  private ImageView fitToScanView;
+  private  AugmentedImageNode foundNode = null;
+  private ModelRenderable mapModel;
+
+  // Augmented image and its associated center pose anchor, keyed by the augmented image in
+  // the database.
+  private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+    fitToScanView = findViewById(R.id.image_view_fit_to_scan);
+
+    arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+    ModelRenderable.builder()
+            .setSource(this, R.raw.initial)
+            .build()
+            .thenAccept(r -> mapModel = r)
+            .exceptionally(throwable -> {
+              Log.e("oops", "Exception loading", throwable);
+              return null;});
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (augmentedImageMap.isEmpty()) {
+      fitToScanView.setVisibility(View.VISIBLE);
+    }
+  }
+
+  /**
+   * Registered with the Sceneform Scene object, this method is called at the start of each frame.
+   *
+   * @param frameTime - time since last frame.
+   */
+  private void onUpdateFrame(FrameTime frameTime) {
+    Frame frame = arFragment.getArSceneView().getArFrame();
+
+    // If there is no frame or ARCore is not tracking yet, just return.
+    if (frame == null || frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
+      return;
+    }
+
+    Collection<AugmentedImage> updatedAugmentedImages =
+        frame.getUpdatedTrackables(AugmentedImage.class);
+
+
+
+    for (AugmentedImage augmentedImage : updatedAugmentedImages) {
+      switch (augmentedImage.getTrackingState()) {
+        case PAUSED:
+          // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+          // but not yet tracked.
+          String text = "Detected Image " + augmentedImage.getIndex();
+          SnackbarHelper.getInstance().showMessage(this, text);
+          break;
+
+        case TRACKING:
+          // Have to switch to UI Thread to update View.
+          fitToScanView.setVisibility(View.GONE);
+
+          // Create a new anchor for newly found images.
+          if (!augmentedImageMap.containsKey(augmentedImage)) {
+            foundNode = new AugmentedImageNode(this);
+            foundNode.setImage(augmentedImage);
+            augmentedImageMap.put(augmentedImage, foundNode);
+            arFragment.getArSceneView().getScene().addChild(foundNode);
+            Vector3 localPosition = new Vector3();
+            localPosition.set(0.0f, 0.0f, 0.0f);
+            Node cornerNode = new Node();
+            cornerNode.setParent(foundNode);
+            Vector3 relativeMapPos = new Vector3(18.5f, -1f, 22f);
+            cornerNode.setLocalPosition(Vector3.add(localPosition, relativeMapPos));
+            Vector3 mapLocalPos = cornerNode.getWorldPosition();
+            Quaternion mapLocalRotation = cornerNode.getWorldRotation();;
+            Node mapNode = new Node();
+            mapNode.setLocalRotation(mapLocalRotation);
+            mapNode.setParent(arFragment.getArSceneView().getScene());
+            mapNode.setLocalPosition(mapLocalPos);
+            mapNode.setRenderable(mapModel);
+          }
+          break;
+
+        case STOPPED:
+          augmentedImageMap.remove(augmentedImage);
+          break;
+      }
+    }
+  }
+}
